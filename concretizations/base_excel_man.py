d59da512956589d6b8340f1b6fc96fb44b88c41a
neo <neo.zhang@1data.info>
2018-09-29 19:56:20 +0800

    package openpyxl simply

--- concretizations/base_excel_man.py
+++ concretizations/base_excel_man.py
@@ -6,47 +6,46 @@ class BaseExcelMan():
     '''
     lines_len = 0
     columns_len = 0
+    workbook = None
     worksheet = None
     char_list = {}
     titles = {}
+    file_path  = None
+
     def __init__(self, file_path, sheet_name=None, has_title=True):
+        self.file_path = file_path
+        self.char_list['A-Z'] = [chr(i) for i in range(65, 91)]
+        self.workbook, self.worksheet = \
+            self.initialization(file_path=file_path, sheet_name=sheet_name)
         if os.path.exists(file_path):
-            self.worksheet = self.initialization(file_path=file_path, sheet_name=sheet_name)
             self.columns_len = self.get_lines_and_columns_num(1)
             self.lines_len = self.get_lines_and_columns_num('A')
             if has_title:
-                self.char_list['A-Z'] = [chr(i) for i in range(65, 91)]
                 self.get_title()
 
     def initialization(self, file_path, sheet_name):
         raise NotImplementedError('{}.parse callback is not defined'.format(self.__class__.__name__))
 
     def get_title(self):
-        '''
-        obtain a dict like as {'title1': 'A'}, and assign the result to the attribute titles
-        '''
-        pass
+        raise NotImplementedError('{}.parse callback is not defined'.format(self.__class__.__name__))
 
     def get_lines_and_columns_num(self, line_or_col):
-        '''
-        obtain the length of lines or columns and return
-        '''
-        pass
+        raise NotImplementedError('{}.parse callback is not defined'.format(self.__class__.__name__))
 
     def get_lines_and_columns(self, line_or_col):
-        '''
-        obtain the lines or columns of excel and return
-        '''
-        pass
+        raise NotImplementedError('{}.parse callback is not defined'.format(self.__class__.__name__))
 
-    def write_one_row(self):
-        pass
+    def write_row(self, row_value_list, istitle=False):
+        raise NotImplementedError('{}.parse callback is not defined'.format(self.__class__.__name__))
 
-    def write_spicified_row(self, specified_row, row_value):
-        pass
+    def write_spicified_row(self, specified_row, row_value_list):
+        raise NotImplementedError('{}.parse callback is not defined'.format(self.__class__.__name__))
 
-    def write_specified_grid(self, specified_grid, value):
-        pass
+    def write_specified_grid(self, value, line, title=None, col_name=None):
+        raise NotImplementedError('{}.parse callback is not defined'.format(self.__class__.__name__))
 
-    def del_specified_row(self,specified_row):
-        pass
+    def del_specified_rows(self, specified_row, del_blank_rows=False):
+        raise NotImplementedError('{}.parse callback is not defined'.format(self.__class__.__name__))
+
+    def del_specified_cols(self, specified_cols, del_blank_cols=False):
+        raise NotImplementedError('{}.parse callback is not defined'.format(self.__class__.__name__))
--- example.py
+++ example.py
@@ -1,4 +1,11 @@
 from concretizations.excel_man_openpyxl import ExcelManOpenPyXl
+import time
+t1 = time.time()
+excel = ExcelManOpenPyXl('test.xlsx', has_title=True)
+# excel.write_row(row_value_list=['是否为板块', '标题', '5'], istitle=True)
 
-excel = ExcelManOpenPyXl('one.xlsx', has_title=True)
-print(excel.get_lines_and_columns(title='title'))
+print(excel.titles)
+excel.del_specified_cols( titles=['链接', '是否扫描'], del_blank_cols=False)
+excel.write_specified_grid(value=500, line=5, title='是否为板块')
+excel.del_specified_rows([7,8], del_blank_rows=True)
+print(time.time()-t1)
